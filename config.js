fddd845c18ce7d87ac2b8a6a86190b6d0c5b5288
Fallsend <susingusmcfury@gmail.com>
2026-01-30 12:37:02 +0530

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://open.spotify.com/track/4UDFr3P1lRS6seQfmzCjz7?si=5d8d147620b94320", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dqwnxjkso/video/upload/v1769756761/Ek_Main_Aur_Ekk_Tu_Amit_Trivedi_320_Kbps_uokca9.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
